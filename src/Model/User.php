4a1d3b8681e4bf5d589c288fc4dcaa569cd91005
Christopher CHEN <chenshaoming@appgame.com>
2016-05-17 22:37:21 +0800

    feat: user remember token storage

--- src/Model/User.php
+++ src/Model/User.php
@@ -19,6 +19,11 @@ class User extends Model
         return $this->setUserProfile(new UserProfile);
     }
 
+    public function getRememberToken()
+    {
+        return ($profile = $this->getUserProfile()) ? $profile->getData('remember_token') : null;
+    }
+
     public function initialize()
     {
         $this->hasOne('id', UserProfile::class, 'user_id', ['alias' => 'user_profile']);
@@ -31,5 +36,24 @@ class User extends Model
             $this->assignUserProfile();
         }
         parent::prepareSave();
+    }
+
+    public function removeRememberToken()
+    {
+        if ($this->getRememberToken()) {
+            $profile = $this->getUserProfile();
+            $profile->setData('remember_token', null);
+            $profile->save();
+        }
+        return $this;
+    }
+
+    public function setRememberToken($rememberToken)
+    {
+        if ($profile = $this->getUserProfile()) {
+            $profile->setData('remember_token', $rememberToken);
+            $profile->save();
+        }
+        return $this;
     }
 }
