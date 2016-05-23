3eb02227cec2874aafca48379a6e2ee4e8f0a414
Christopher CHEN <chenshaoming@appgame.com>
2016-05-23 21:20:43 +0800

    fix: router compatible with sub path

--- src/Router.php
+++ src/Router.php
@@ -29,10 +29,16 @@ class Router extends PhalconRouter
      */
     protected static $router;
     protected $_uriSource = self::URI_SOURCE_SERVER_REQUEST_URI;
+    protected $_sitePathPrefix;
+    protected $_sitePathLength;
 
     public function __construct()
     {
         parent::__construct(false);
+        if ($this->_sitePathPrefix = Config::get('app.site_path')) {
+            $this->_uriSource = self::URI_SOURCE_GET_URL;
+            $this->_sitePathLength = strlen($this->_sitePathPrefix);
+        }
         $this->removeExtraSlashes(true);
         $routes = is_file($file = static::$di['ROOT_PATH'] . '/app/routes.php') ? include $file : [];
         is_array($routes) and $this->addRoutes($routes);
@@ -74,6 +80,11 @@ class Router extends PhalconRouter
     {
         static::$router === null and static::$router = static::$di->getShared('router');
         $router = static::$router;
+        if (!$uri && $router->_sitePathLength && $router->_uriSource == self::URI_SOURCE_GET_URL) {
+            list($uri) = explode('?', $_SERVER['REQUEST_URI']);
+            $uri = str_replace(basename($_SERVER['SCRIPT_FILENAME']), '', $uri);
+            substr($uri, 0, $router->_sitePathLength) == $router->_sitePathPrefix and $uri = substr($uri, $router->_sitePathLength);
+        }
         $router->handle($uri);
         if ($route = $router->getMatchedRoute()) {
             static::$disableSession or Session::start();
