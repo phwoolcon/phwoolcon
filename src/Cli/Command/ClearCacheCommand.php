0a51f30ef1719264b5eff9898cda99e863152dac
Christopher CHEN <chenshaoming@appgame.com>
2016-06-12 21:41:50 +0800

    feat: add cache clear command

---
+++ src/Cli/Command/ClearCacheCommand.php
@@ -0,0 +1,51 @@
+<?php
+namespace Phwoolcon\Cli\Command;
+
+use Phwoolcon\Cache;
+use Phwoolcon\Cli\Command;
+use Phwoolcon\Config;
+use Phwoolcon\Db;
+use Phwoolcon\I18n;
+use Phwoolcon\View;
+use Symfony\Component\Console\Input\InputOption;
+
+class ClearCacheCommand extends Command
+{
+
+    protected function configure()
+    {
+        $this->setDefinition([
+            new InputOption('config-only', 'c', InputOption::VALUE_NONE, 'Clear config cache only'),
+            new InputOption('meta-only', 'm', InputOption::VALUE_NONE, 'Clear model metadata only'),
+            new InputOption('locale-only', 'l', InputOption::VALUE_NONE, 'Clear locale cache only'),
+            new InputOption('assets-only', 'a', InputOption::VALUE_NONE, 'Clear assets cache only'),
+        ])->setDescription('Clears cache');
+    }
+
+    public function fire()
+    {
+        if ($this->input->getOption('config-only')) {
+            Config::clearCache();
+            $this->info('Config cache cleared.');
+            return;
+        }
+        if ($this->input->getOption('meta-only')) {
+            Db::clearMetadata();
+            $this->info('Model metadata cleared.');
+            return;
+        }
+        if ($this->input->getOption('locale-only')) {
+            I18n::clearCache();
+            $this->info('Locale cache cleared.');
+            return;
+        }
+        if ($this->input->getOption('assets-only')) {
+            View::clearAssetsCache();
+            $this->info('Assets cache cleared.');
+            return;
+        }
+        Cache::flush();
+        Config::clearCache();
+        $this->info('Cache cleared.');
+    }
+}
---
+++ tests/Integration/Cli/MigrationTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Phwoolcon\Tests\Unit\Cli;
+
+use Phwoolcon\Db;
+use Phwoolcon\Tests\CliTestCase;
+
+class MigrationTest extends CliTestCase
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+}
