0f8929b1b99bd118da9d867073b6e73b5cfc02a7
Christopher CHEN <chenshaoming@appgame.com>
2016-08-17 22:23:49 +0800

    test: clear log in phpunit entrance instead of start script

--- tests/start.php
+++ tests/start.php
@@ -19,6 +19,3 @@ $_SERVER['PHWOOLCON_CONFIG_PATH'] = TEST_ROOT_PATH . '/app/config';
 
 // Register class loader
 include __DIR__ . '/../vendor/autoload.php';
-
-is_file($logFile = TEST_ROOT_PATH . '/storage/logs/phwoolcon.log') and file_put_contents($logFile, '');
-is_file($logFile = TEST_ROOT_PATH . '/storage/logs/service.log') and file_put_contents($logFile, '');
