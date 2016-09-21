d666d4531ae699e23e3a4fb5887ad09247d05fd2
Christopher CHEN <chenshaoming@appgame.com>
2016-09-21 22:08:43 +0800

    feat: install class aliases via update-package-resource script

--- phwoolcon-package/commands.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'clear:cache' => 'Phwoolcon\Cli\Command\ClearCacheCommand',
-    'migrate' => 'Phwoolcon\Cli\Command\Migrate',
-    'migrate:create' => 'Phwoolcon\Cli\Command\MigrateCreate',
-    'migrate:revert' => 'Phwoolcon\Cli\Command\MigrateRevert',
-    'migrate:list' => 'Phwoolcon\Cli\Command\MigrateList',
-    'service' => 'Phwoolcon\Cli\Command\ServiceCommand',
-];
--- phwoolcon-package/phwoolcon-package-phwoolcon.php
+++ phwoolcon-package/phwoolcon-package-phwoolcon.php
@@ -16,5 +16,31 @@ return [
         'di' => [
             0 => 'di.php',
         ],
+        'commands' => [
+            0 => [
+                'clear:cache' => 'Phwoolcon\Cli\Command\ClearCacheCommand',
+                'migrate' => 'Phwoolcon\Cli\Command\Migrate',
+                'migrate:create' => 'Phwoolcon\Cli\Command\MigrateCreate',
+                'migrate:revert' => 'Phwoolcon\Cli\Command\MigrateRevert',
+                'migrate:list' => 'Phwoolcon\Cli\Command\MigrateList',
+                'service' => 'Phwoolcon\Cli\Command\ServiceCommand',
+            ],
+        ],
+        'class_aliases' => [
+            0 => [
+                'Config' => 'Phwoolcon\Config',
+                'Db' => 'Phwoolcon\Db',
+                'I18n' => 'Phwoolcon\I18n',
+                'Log' => 'Phwoolcon\Log',
+                'Queue' => 'Phwoolcon\Queue',
+                'Router' => 'Phwoolcon\Router',
+                'Session' => 'Phwoolcon\Session',
+                'View' => 'Phwoolcon\View',
+                'User' => 'Phwoolcon\Model\User',
+                'DisableSessionFilter' => 'Phwoolcon\Router\Filter\DisableSessionFilter',
+                'DisableCsrfFilter' => 'Phwoolcon\Router\Filter\DisableCsrfFilter',
+                'MultiFilter' => 'Phwoolcon\Router\Filter\MultiFilter',
+            ],
+        ],
     ],
 ];
