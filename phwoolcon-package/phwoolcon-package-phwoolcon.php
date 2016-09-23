9467f8e6256c21630352f432f53d9e47f396c8b2
Christopher CHEN <chenshaoming@appgame.com>
2016-09-23 15:09:03 +0800

    refactor: remove config section in phwoolcon package

--- phwoolcon-package/phwoolcon-package-phwoolcon.php
+++ phwoolcon-package/phwoolcon-package-phwoolcon.php
@@ -1,18 +1,6 @@
 <?php
 return [
     'phwoolcon/phwoolcon' => [
-        'config' => [
-            'app.php',
-            'cache.php',
-            'commands.php',
-            'cookies.php',
-            'database.php',
-            'i18n.php',
-            'queue.php',
-            'service.php',
-            'session.php',
-            'view.php',
-        ],
         'di' => [
             0 => 'di.php',
         ],
