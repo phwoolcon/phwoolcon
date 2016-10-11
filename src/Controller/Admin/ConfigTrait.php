f09b053c6f7abe46b36a3f1b750f527f4cabc4e7
Christopher CHEN <chenshaoming@appgame.com>
2016-10-11 20:49:58 +0800

    feat(admin): config modification trait

--- src/Controller/Admin/ConfigTrait.php
+++ src/Controller/Admin/ConfigTrait.php
@@ -1,14 +1,37 @@
 <?php
 namespace Phwoolcon\Controller\Admin;
 
+use Phalcon\Validation\Exception as ValidationException;
+use Phwoolcon\Config;
+use Phwoolcon\Model\Config as ConfigModel;
+
 trait ConfigTrait
 {
 
-    public function getConfig()
+    protected function filterSubmittedConfig($key, $data)
     {
+        unset($data['_sensitive_keys']);
+        if (is_array($sensitiveKeys = Config::get($key . '._sensitive_keys'))) {
+            foreach ($sensitiveKeys as $key) {
+                if ($key == '*') {
+                    throw new ValidationException("Config group '{$key}' is protected");
+                }
+                array_forget($data, $key);
+            }
+        }
+        return $data;
     }
 
-    public function postConfig()
+    protected function submitConfig($key, $data)
     {
+        if (is_string($data)) {
+            $data = json_decode($data, true);
+            if (json_last_error() != JSON_ERROR_NONE) {
+                throw new ValidationException(json_last_error_msg(), json_last_error());
+            }
+        }
+        $value = $this->filterSubmittedConfig($key, $data);
+        ConfigModel::saveConfig($key, $value);
+        Config::clearCache();
     }
 }
