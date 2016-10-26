a35ffbd4813100d9f51788717f6cdbd7f142867e
Christopher CHEN <chenshaoming@appgame.com>
2016-10-26 15:44:58 +0800

    feat(i18n): config key label translation

---
+++ phwoolcon-package/locale/en/phwoolcon.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'config_key_app' => 'General',
+    'config_key_cookies' => 'Cookies',
+    'config_key_database' => 'Database',
+    'config_key_i18n' => 'Internationalization',
+    'config_key_session' => 'Session',
+    'config_key_view' => 'View',
+];
---
+++ phwoolcon-package/locale/zh_CN/phwoolcon.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'config_key_app' => '一般设置',
+    'config_key_cookies' => 'Cookies 设置',
+    'config_key_database' => '数据库设置',
+    'config_key_i18n' => '国际化设置',
+    'config_key_session' => 'Session 设置',
+    'config_key_view' => '视图设置',
+];
