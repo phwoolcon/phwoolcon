d1dfe88b5b122ab0280883e596f90c13ca076462
Christopher CHEN <fishdrowned@gmail.com>
2017-10-18 14:43:31 +0800

    docs: specify the source of `console-fix.js` [ci skip]

--- phwoolcon-package/assets/phwoolcon/ie/console-fix.js
+++ phwoolcon-package/assets/phwoolcon/ie/console-fix.js
@@ -1,3 +1,4 @@
+/* @see https://stackoverflow.com/a/5539378/802646 */
 if (Function.prototype.bind && window.console && typeof console.log == "object") {
     [
         "log", "info", "warn", "error", "assert", "dir", "clear", "profile", "profileEnd"
