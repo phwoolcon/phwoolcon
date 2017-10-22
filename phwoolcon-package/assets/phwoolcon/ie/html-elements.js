7112ffa6236c9cc0d01f3c0a96efe3ec362f9645
Christopher CHEN <fishdrowned@gmail.com>
2017-10-22 13:59:02 +0800

    feat(assets): update ie hack js [ci skip]
    polyfill: Element.prototype.matches

--- phwoolcon-package/assets/phwoolcon/ie/html-elements.js
+++ phwoolcon-package/assets/phwoolcon/ie/html-elements.js
@@ -1,2 +1,17 @@
 window.HTMLElement || (HTMLElement = Element);
 document.head || (document["head"] = document.getElementsByTagName("head")[0]);
+if (!Element.prototype.matches) {
+    Element.prototype.matches =
+        Element.prototype.matchesSelector ||
+        Element.prototype.mozMatchesSelector ||
+        Element.prototype.msMatchesSelector ||
+        Element.prototype.oMatchesSelector ||
+        Element.prototype.webkitMatchesSelector ||
+        function (s) {
+            var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                i = matches.length;
+            while (--i >= 0 && matches.item(i) !== this) {
+            }
+            return i > -1;
+        };
+}
